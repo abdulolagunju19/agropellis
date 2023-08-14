68b3342b24e03e09f56d166c416a29d835a37c56
Abdul-Samad Olagunju <abdulolagunju19@gmail.com>
2023-08-14 00:09:59 -0600

    Add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,17 @@
-import './globals.css'
+import './globals.css';
 
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Agropellis',
   description: 'Buy the best organic crops for great skin.',
 }
 
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import Header from './Header'
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
+import Header from './Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         {/* div flex-1 Push footer content to the bottom of the page */}
         <div className='flex-1'>
           {children}
+          <Analytics />
         </div>
         <footer className='flex items-center flex-wrap justify-center border-t border-solid border-slate-300 p-4 md:p-8'>
           <p className='px-2'>Olagunju 2023</p>
